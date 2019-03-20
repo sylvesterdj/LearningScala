234ed84c39fde4078a66e2aa7c92267f7dadb31d
Sylvester Daniel <sylvester.john@marriott.com>
2019-03-20 20:22:56 +0530

    Moved multi stream.

--- SparkStreamingPOC/src/main/scala/streaming/Rate2Kafka.scala
+++ SparkStreamingPOC/src/main/scala/streaming/Rate2Kafka.scala
@@ -12,27 +12,19 @@ object Rate2Kafka extends App {
 
   val df = spark.readStream
     .format("rate")
-    .option("rowsPerSecond", 150000)
-    .option("numPartitions", 10)
+    .option("rowsPerSecond", 2)
+    .option("numPartitions", 2)
     .option("rampUpTime", 1)
     .load()
 
   val rateRawData = df.selectExpr("CAST(timestamp AS STRING)", "CAST(value AS string)")
-  val kafkaWriteStream1 = rateRawData.writeStream
+  val kafkaStream = rateRawData.writeStream
     .format("kafka")
     .queryName("First Kafka Stream")
     .option("topic", "test2")
-    .option("checkpointLocation", "C:\\sparkCheckPoint\\Rate2Kafa\\cp1").option("kafka.bootstrap.servers", "localhost:9092")
+    .option("checkpointLocation", "C:\\sparkCheckPoint\\Rate2Kafka\\cp1").option("kafka.bootstrap.servers", "localhost:9092")
     .trigger(Trigger.ProcessingTime("10 seconds"))
     .start()
 
-  val kafkaWriteStream2 = rateRawData.writeStream
-    .format("kafka")
-    .queryName("Second Kafka Stream")
-    .option("topic", "test2")
-    .option("checkpointLocation", "C:\\sparkCheckPoint\\Rate2Kafa\\cp2").option("kafka.bootstrap.servers", "localhost:9092")
-    .trigger(ProcessingTime("20 seconds"))
-    .start()
-
-  spark.streams.awaitAnyTermination()
+  kafkaStream.awaitTermination()
 }
---
+++ SparkStreamingPOC/src/main/scala/streaming/Rate2KafkaMultiStream.scala
@@ -0,0 +1,38 @@
+package streaming
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.{ProcessingTime, Trigger}
+
+object Rate2KafkaMultiStream extends App {
+
+  val spark: SparkSession = SparkSession.builder()
+    .appName("Rate2KafkaMultiStream")
+    .master("local[*]")
+    .getOrCreate()
+
+  val df = spark.readStream
+    .format("rate")
+    .option("rowsPerSecond", 20000)
+    .option("numPartitions", 2)
+    .option("rampUpTime", 1)
+    .load()
+
+  val rateRawData = df.selectExpr("CAST(timestamp AS STRING)", "CAST(value AS string)")
+  val kafkaWriteStream1 = rateRawData.writeStream
+    .queryName("First Kafka Stream")
+    .format("kafka")
+    .option("topic", "test2")
+    .option("checkpointLocation", "C:\\sparkCheckPoint\\Rate2KafkaMultiStream\\cp1").option("kafka.bootstrap.servers", "localhost:9092")
+    .trigger(Trigger.ProcessingTime("10 seconds"))
+    .start()
+
+  val kafkaWriteStream2 = rateRawData.writeStream
+    .queryName("Second Kafka Stream")
+    .format("kafka")
+    .option("topic", "test2")
+    .option("checkpointLocation", "C:\\sparkCheckPoint\\Rate2KafkaMultiStream\\cp2").option("kafka.bootstrap.servers", "localhost:9092")
+    .trigger(Trigger.ProcessingTime("10 seconds"))
+    .start()
+
+  spark.streams.awaitAnyTermination()
+}
